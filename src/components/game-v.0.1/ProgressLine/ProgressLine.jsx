3d96feaad4d574ddd6f5de462b6797bca3c77b31
vano <kolesen.ivan@gmail.com>
2018-10-13 15:00:27 +0300

    perf: create progress line

--- src/components/game-v.0.1/ProgressLine/ProgressLine.jsx
+++ src/components/game-v.0.1/ProgressLine/ProgressLine.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 import './ProgressLine.css';
 
 const ProgressLine = () => {
-  return <div className="progress-line">progressLine</div>;
+  return (
+    <div className="progress-line">
+      <div className="progress-line__item">
+        <FontAwesomeIcon icon="circle" color="#0cb3c6" />
+      </div>
+      <div className="progress-line__item">
+        <FontAwesomeIcon icon="circle" color="#0cb3c6" />
+      </div>
+      <div className="progress-line__item">
+        <FontAwesomeIcon icon="star" size="2x" color="#fdd14c" />
+      </div>
+      <div className="progress-line__item">
+        <FontAwesomeIcon icon="star" size="2x" color="#fdd14c" />
+      </div>
+      <div className="progress-line__item">
+        <FontAwesomeIcon icon="star" size="2x" color="#fdd14c" />
+      </div>
+    </div>
+  );
 };
 
 export default ProgressLine;
