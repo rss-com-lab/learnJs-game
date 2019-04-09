b7c245c5c431df48d57a64b97538b8cf3b6c41e6
AntiHero <prophreaker@gmail.com>
2019-04-09 13:19:43 +0300

    game-343-question list generation(fix)

--- src/api/questions.js
+++ src/api/questions.js
@@ -3,126 +3,37 @@ export function generateQuestionsList(
   config,
   numberOfQuestions,
 ) {
+  let questions = [];
   let questionsList = [];
-  //let operators = config.complexity[complexityLevel].operators;
-  //let maxNumber = config.complexity[complexityLevel].maxNumber;
-  //let firstNumber, secondNumber, operator;
+  let result = {};
 
-  for (let i = 1; i <= numberOfQuestions; i++) {
-    let result = {};
-    // let remainder = i % 3;
+  for (let i = 0; i < config.questionType.open.data.length; i++) {
     if (
-      complexityLevel * Number(config.questionType.open.data[i].complexity) ===
+      complexityLevel / Number(config.questionType.open.data[i].complexity) ===
       1
     ) {
-      //operator = operators[Math.floor(Math.random() * operators.length)];
-      let randomOpen = Math.floor(
-        Math.random() * config.questionType.open.data.length,
-      );
-      console.log('here');
-      result.questionType = 'open';
-      result.correctAnswer =
-        config.questionType.open.data[randomOpen].correctAnswer;
-      result.responseTime = config.questionType['open'].responseTime;
-      result.questionTitle =
-        config.questionType.open.data[randomOpen].questionTitle;
-      result.question = [];
-      //result.question =
-      //config.questionType.open.data[randomOpen].questionDescription[0];
-      result.explanation =
-        config.questionType.open.data[randomOpen].explanation;
-
-      for (
-        let j = 0;
-        j <
-        config.questionType.open.data[randomOpen].questionDescription.length;
-        j++
-      ) {
-        result.question.push(
-          config.questionType.open.data[randomOpen].questionDescription[j],
-        );
-      }
-
-      console.log(result);
-      // if (operator === ':') {
-      //   secondNumber = Math.floor(Math.random() * (maxNumber / 2) + 1);
-      //   firstNumber = secondNumber * 2;
-      //   result.correctAnswer = firstNumber / secondNumber;
-      // }
-
-      // if (operator === 'x') {
-      //   firstNumber = Math.floor(Math.random() * maxNumber + 1);
-      //   secondNumber = 2;
-      //   result.correctAnswer = firstNumber * secondNumber;
-      // }
-
-      // if (operator === '+') {
-      //   firstNumber = Math.floor(Math.random() * maxNumber);
-      //   secondNumber = Math.floor(Math.random() * maxNumber);
-      //   result.correctAnswer = firstNumber + secondNumber;
-      // }
-
-      // result.question =
-      //   'Cколько будет ' +
-      //   firstNumber +
-      //   ' ' +
-      //   operator +
-      //   ' ' +
-      //   secondNumber +
-      //   '?';
-
-      // if (operator === '-') {
-      //   firstNumber = Math.floor(Math.random() * maxNumber);
-      //   secondNumber = Math.floor(Math.random() * maxNumber);
-      //   if (secondNumber >= firstNumber) {
-      //     result.correctAnswer = secondNumber - firstNumber;
-      //     result.question =
-      //       'Cколько будет ' +
-      //       secondNumber +
-      //       ' ' +
-      //       operator +
-      //       ' ' +
-      //       firstNumber +
-      //       '?';
-      //   } else {
-      //     result.correctAnswer = firstNumber - secondNumber;
-      //     result.question =
-      //       'Cколько будет ' +
-      //       firstNumber +
-      //       ' ' +
-      //       operator +
-      //       ' ' +
-      //       secondNumber +
-      //       '?';
-      //   }
-      //}
-      // } else {
-      //   let randomSelective = Math.floor(
-      //     Math.random() * config.questionType.selective.data.length,
-      //   );
-      //   let randomDescriptive = Math.floor(
-      //     Math.random() * config.questionType.descriptive.data.length,
-      //   );
-      //   result = {
-      //     question: config.questionType.selective.data[randomSelective].question,
-      //     answers: config.questionType.selective.data[randomSelective].answers,
-      //     correctAnswer:
-      //       config.questionType.selective.data[randomSelective].correctAnswer,
-      //     questionType: 'selective',
-      //     responseTime: config.questionType['selective'].responseTime,
-      //   } || {
-      //     question:
-      //       config.questionType.descriptive.data[randomDescriptive].question,
-      //     answers:
-      //       config.questionType.descriptive.data[randomDescriptive].answers,
-      //     correctAnswer:
-      //       config.questionType.descriptive.data[randomDescriptive].correctAnswer,
-      //     questionType: 'descriptive',
-      //     responseTime: config.questionType['descriptive'].responseTime,
-      //   };
-      // }
-      questionsList.push(result);
+      questions.push(config.questionType.open.data[i]);
     }
   }
+
+  console.log(questions);
+  for (let i = 1; i <= numberOfQuestions; i++) {
+    let randomOpen = Math.floor(Math.random() * questions.length);
+    console.log(randomOpen);
+    result.questionType = 'open';
+    result.correctAnswer = questions[randomOpen].correctAnswer;
+    result.responseTime = config.questionType['open'].responseTime;
+    result.questionTitle = questions[randomOpen].questionTitle;
+    result.question = [];
+    result.explanation = questions[randomOpen].explanation;
+
+    for (let j = 0; j < questions[randomOpen].questionDescription.length; j++) {
+      result.question.push(questions[randomOpen].questionDescription[j]);
+    }
+    questionsList.push(result);
+    result = {};
+  }
+
+  console.log(questionsList);
   return questionsList;
 }
